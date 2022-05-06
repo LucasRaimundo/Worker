4d573e5ab55bcd2be1445af24d7de78aa1600096
Lucas Raimundo <lucasraimundo2010@gmail.com>
2022-05-06 13:12:22 -0300

    Incrementando no sistema principal informações departamentais

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entities.Department;
+import entities.HourContract;
 import entities.Worker;
 import entities.enums.WorkerLevel;
 
@@ -19,7 +20,7 @@ public class Program {
 		
 		System.out.print("Enter department's name: ");
 		String departmentName = ler.nextLine();
-		System.out.print("Enter worker data: ");
+		System.out.println("Enter worker data: ");
 		System.out.print("Name: ");
 		String workerName = ler.nextLine();
 		System.out.print("Level: ");
@@ -30,11 +31,26 @@ public class Program {
 		
 		System.out.print("Hoy many contracts to this worker? ");
 		int n = ler.nextInt();
-		for(int i =0; i<=n;i++) {
-			System.out.println("Enter contract #" + i + "data:");
+		for(int i =1; i<=n;i++) {
+			System.out.println("Enter contract #" + i + " data:");
 			System.out.print("Date (DD/MM/YYYY: ");
 			Date contractDate = sdf.parse(ler.next());
+			System.out.print("Value per Hour: ");
+			double valuePerHour = ler.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = ler.nextInt();
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			worker.addContract(contract);
 		}
+		System.out.println();
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		String monthAndYear = ler.next();
+		int month = Integer.parseInt(monthAndYear.substring(0, 2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Department: " + worker.getDepartment().getName());
+		System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.incame(year, month)));
+		
 		
 		ler.close();
 	}
